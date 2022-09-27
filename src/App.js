93fc32b241005faa16bd9b9a29adf65efbc19f40
Aidan Killeen <aidan.killeen@gmail.com>
2022-09-27 11:59:53 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
         <h1>React App With Authentication</h1>
-        <h2>Deployed to Firebase</h2>
+        <nav style={{
+            borderBottom: 'solid 1px', 
+            paddingBottom: '1rem'
+        }}
+        >
+            <Link to="/">Home</Link> |{" "}
+            <Link to="/about">About</Link> |{" "}
+            <Link to="/contact">Contact Us</Link>
+        </nav>
+        <Outlet/>
     </div>
   );
 }
